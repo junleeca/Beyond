120596bb3684cd02ed798b4186239a85259b3cfc
hamedfathi <hamedfathi@outlook.com>
2022-08-15 13:42:02 +0200

    chore(Project): more extensions added.

--- Beyond/Extensions/StringExtensions.cs
+++ Beyond/Extensions/StringExtensions.cs
@@ -13,9 +13,11 @@ public static class StringExtensions
 
     public static bool MatchWords(this string text, string word)
     {
-        var tokens = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim().ToLower());
+        var tokens = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.Trim().ToLower());
         return tokens.Any(t => word.Trim().ToLower().Contains(t.Trim()));
     }
+
     public static string TabOrWhiteSpaceHandler(this string text,
         StringSplitOptions stringSplitOptions = StringSplitOptions.None)
     {
@@ -532,6 +534,29 @@ public static class StringExtensions
         }
 
         return Encoding.UTF8.GetString(buffer);
+    }
+
+    public static Encoding GetEncoding(string filePath)
+    {
+        // Read the BOM
+        var bom = new byte[4];
+        using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        {
+            var _ = file.Read(bom, 0, 4);
+        }
+
+        // Analyze the BOM
+        if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+        if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+        if (bom[0] == 0xff && bom[1] == 0xfe && bom[2] == 0 && bom[3] == 0) return Encoding.UTF32; //UTF-32LE
+        if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+        if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+        if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff)
+            return new UTF32Encoding(true, true); //UTF-32BE
+
+        // We actually have no idea what the encoding is if we reach this point, so
+        // you may wish to return null instead of defaulting to ASCII
+        return Encoding.ASCII;
     }
 
     public static bool HasMultipleInstancesOf(this string input, char charToFind)
@@ -1134,7 +1159,7 @@ public static class StringExtensions
             if (unicodeCategory != UnicodeCategory.NonSpacingMark)
                 stringBuilder.Append(c);
             else
-            //اسامی مانند آفتاب نباید خراب شوند
+                //اسامی مانند آفتاب نباید خراب شوند
             if (c == 1619) //آ
                 stringBuilder.Append(c);
         }
---
+++ Beyond/Utilities/CurrentFileUtility.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+
+namespace Beyond.Utilities;
+
+public static class CurrentFileUtility
+{
+    public static string Path([CallerFilePath] string file = "") => file;
+    public static string Directory([CallerFilePath] string file = "") => System.IO.Path.GetDirectoryName(file)!;
+    public static string Relative(string relative, [CallerFilePath] string file = "")
+    {
+        var directory = System.IO.Path.GetDirectoryName(file)!;
+        return System.IO.Path.Combine(directory, relative);
+    }
+}
